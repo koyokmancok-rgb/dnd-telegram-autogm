8c87bd5fcd55fd3943a547ce954e8159b533011e
koyokmancok-rgb <koyokmancok@gmail.com>
2025-08-15 15:27:40 +0800

    Create abilities.py

---
+++ abilities.py
@@ -0,0 +1,7 @@
+{
+  "Slash": { "dmg": "1d6+2", "type": "slashing", "toHitMod": "+0", "aoe": false, "desc": "Serangan pedang sederhana" },
+  "Fireball": { "dmg": "6d6", "type": "fire", "toHitMod": null, "aoe": true, "desc": "Ledakan api area (AOE)" },
+  "Heal": { "heal": "2d8+3", "aoe": false, "desc": "Penyembuhan" },
+  "Lightning": { "dmg": "4d6", "type": "lightning", "toHitMod": "+2", "aoe": false, "desc": "Sengatan petir" },
+  "Cleave": { "dmg": "1d8+2", "type": "slashing", "toHitMod": "+1", "aoe": true, "desc": "Serangan memukul beberapa musuh" }
+}
